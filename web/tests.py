fda44684d3ee306477110df9c27611ea664b0062
Gláuber Brennon <glauberbrennon@gmail.com>
2015-02-14 05:15:30 -0200

    a test that doesnt pass

--- web/tests.py
+++ web/tests.py
@@ -6,9 +6,10 @@ class MainTestCase(unittest.TestCase):
     def test_two_and_two(self):
         four = 2 + 2
         self.assertEqual(four, 4)
-        self.assertNotEqual(four, 5)
-        self.assertNotEqual(four, 6)
 
+    def test_two_and_three(self):
+        five = 3 + 3
+        self.assertEqual(five, 5)
 
 if __name__ == '__main__':
     unittest.main()
